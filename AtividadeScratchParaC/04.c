3c88ad7d2be260d49e3e7fb1a9d79139a634b801
Arthur Mendes Lima <1590014@sga.pucminas.br>
2025-02-28 08:30:33 -0300

    Primeiro

---
+++ AtividadeScratchParaC/04.c
@@ -0,0 +1,14 @@
+//Tarefa - Ex04 Desafio Calculadora de Somas
+#include <stdio.h>
+int main(void){
+    float n1,n2,n3,soma;
+        
+    printf("Digite o 1 Valor: \n");
+    scanf("%f",&n1);
+    printf("Digite o 2 Valor: \n");
+    scanf("%f",&n2);
+    printf("Digite o 3 Valor: \n");
+    scanf("%f",&n3);
+soma = n1+n2+n3;
+    printf("Soma de %.1f+%.1f+%.1f=%.1f",n1,n2,n3,soma);
+    return 1;}
